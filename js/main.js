13a33272c3421d3c6d771c05117ca1687970e26d
Jamie Cranston <jamesc@raicovdesigns.com.au>
2015-03-13 12:33:45 +1100

    Begun setting up basic functionality
    Can now click numbers and have them appear in the textBar, Operators are
    still no operational in this version

--- js/main.js
+++ js/main.js
@@ -4,9 +4,20 @@ $(window).load(function(){
         if(e.keyName == "back")
             tizen.application.getCurrentApplication().exit();
     });
-	
-	
-	$('.contents').on("click", function(){
-		$('#textbox').html($('#textbox').html() == "Basic" ? "Sample" : "Basic");				
-	});
+	totaldiv = $('.textBar');
+	var testNumLength = function(number){
+      if(number.length > 9){
+          totaldiv.text(number.substr(number.length-9, 9));
+      }
+    };
+    var number = "";
+    var newnumber = "";
+    var operator = "";
+    totaldiv.text("0");
+    
+    $(".button").not('.C, .AC').click(function(){
+        number += $(this).text();
+        totaldiv.text(number);
+        testNumLength(number);
+    });
 });
