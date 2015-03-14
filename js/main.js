810d8ed1641afded8e8754d7bb07815c5780ba59
Jamie <james.cranston91@gmail.com>
2015-03-14 19:04:29 +1100

    Completed main operators,second page started + more
    All main operators function correctly, allowed switching of operators
    after already selecting one and moving to the second number in equation,
    fixed textBar across both pages. Next up: more styling and Row order
    switching function.

--- js/main.js
+++ js/main.js
@@ -8,29 +8,60 @@ $(window).load(function(){
     var number = "";
     var newnumber = "";
     var operator = "";
+    var operatorSet = false;
+    var calculation = "";
     totaldiv.text("0");
     
     $(".button").not('.C, .AC').click(function(){
         number += $(this).text();
         totaldiv.text(number);
     });
-    $('.operator').click(function(){
-       if(operator.length > 0){
-           operator = $(this).html();
-       }else{
+    $('.operator').not('.equals').click(function(){
+        if(!operatorSet){
         newnumber = totaldiv.text();
         number= "";
         totaldiv.text('0');
         operator = $(this).html();
+        operatorSet = true;
+        }
+       if(operatorSet){
+           operator = $(this).html();
        }
-        
     });
     $('.C, .AC').click(function(){
           var whichClass = $(this).attr('class');
         number = "";
         totaldiv.text('0')
-        if(whichClass == "AC"){
-            newnumber = "";   
+        if(whichClass === "AC"){
+            newnumber = ""; 
+            calculation = "";
+            operator = "";
+            operatorSet = false;
         }
     });
+    $('.equals').click(function(){
+           if(operator == '+'){
+                 calculation = (parseFloat(newnumber,10))+(parseFloat(number,10));
+                   totaldiv.text(calculation.toString()); 
+                   operator = "";
+                   operatorSet = false;
+           }else if(operator == "-"){
+                 calculation = (parseFloat(newnumber,10))-(parseFloat(number,10));
+                   totaldiv.text(calculation.toString()); 
+                   operator = "";
+                   operatorSet = false;
+            }else if(operator == "×"){
+                 calculation = (parseFloat(newnumber,10))*(parseFloat(number,10));
+                   totaldiv.text(calculation.toString()); 
+                   operator = "";
+                   operatorSet = false;
+           }else if(operator == "÷"){
+                 calculation = (parseFloat(newnumber,10))/(parseFloat(number,10));
+                   totaldiv.text(calculation.toString()); 
+                   operator = "";
+                   operatorSet = false;
+           };
+            
+    });
+    
 });
